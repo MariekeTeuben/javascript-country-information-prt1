c2262a6466894a199a06a4daebbfabb9383a9cb1
MariekeTeuben <marieke@marenmaa.nl>
2022-08-17 16:12:20 +0200

    Data fetchen via API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('Hallo daar!');
+import axios from 'axios';
+
+const countryList = document.getElementById('list-of-countries');
+
+
+async function getCountryInformation() {
+
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+        console.log(result.data);
+
+        countryList.innerHTML = `
+        <li>
+            <img src=" ${result.data[0].flag}" alt="country-flag"/>
+            ${result.data[0].name}
+            <p>Has a population of ${result.data[0].population} people</p> 
+        </li>
+        `
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+getCountryInformation();
+
